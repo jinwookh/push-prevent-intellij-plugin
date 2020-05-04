b3245bf99da4d1ca70c50dd5c5e68d3453b178ca
jinwook han <jin942002@naver.com>
2020-05-04 12:28:05 +0900

    first commit

---
+++ src/main/java/SomeAction.java
@@ -0,0 +1,13 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.SQLException;
+
+public class SomeAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+}
